7d19bbaca7e28f7500b85929ea324b9de4e24704
Ccw <kidoccw@gmail.com>
2025-06-01 23:37:54 +0800

    style: Some padding adjustment

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,13 +8,13 @@ import { Separator } from "@/components/ui/separator";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex justify-end items-center px-5 h-[80px]">
+      <div className="flex justify-end items-center px-10 h-[80px]">
         <ThemeButton />
       </div>
 
       <Separator />
 
-      <div className="grid grid-cols-[280px_1fr] gap-5 flex-1">
+      <div className="grid grid-cols-[280px_1fr] flex-1 px-5">
         <SideBar />
         <Dashboard className="max-h-[calc(100vh-81px)]" />
       </div>
