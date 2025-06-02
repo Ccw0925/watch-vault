d41d7f8a3010edacef4cd2958893e907dec169b3
Ccw <kidoccw@gmail.com>
2025-06-02 23:41:00 +0800

    refactor(frontend): Remove unused code

--- frontend/src/components/home/Dashboard.tsx
+++ frontend/src/components/home/Dashboard.tsx
@@ -2,15 +2,6 @@ import React from "react";
 import HighlightsBanner from "./HighlightsBanner";
 import { twMerge } from "tailwind-merge";
 import ContinueWatching from "./ContinueWatching";
-// import {
-//   TypographyH1,
-//   TypographyH4,
-//   TypographyP,
-// } from "@/components/ui/typography";
-// import { motion } from "motion/react";
-// import Link from "next/link";
-// import { Button } from "@/components/ui/button";
-// import Image from "next/image";
 
 const Dashboard = ({ className }: { className?: string }) => {
   return (
