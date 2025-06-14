34e635b1439ccca78ded8f420f72265bdc65a5a4
Ccw <kidoccw@gmail.com>
2025-06-14 18:42:23 +0800

    refactor(frontend): Code refactoring

--- frontend/src/app/animes/[id]/AnimeDetailsPage.tsx
+++ frontend/src/app/animes/[id]/AnimeDetailsPage.tsx
@@ -86,31 +86,14 @@ const AnimeDetailsPage = () => {
           <div className="md:col-span-2 space-y-6 order-2 lg:order-none">
             <AnimeTitleInfo anime={anime} />
             <AnimeStatsBadges anime={anime} />
-
-            <div className="flex flex-col gap-2">
-              <TypographyP
-                ref={synopsisRef}
-                className={`text-justify whitespace-pre-line line-clamp-7 ${
-                  showMore && "line-clamp-none"
-                }`}
-              >
-                {anime.synopsis}
-              </TypographyP>
-
-              {shouldShowMore && (
-                <div className="flex flex-col items-center">
-                  <Button
-                    className="font-inter dark:bg-input/30 dark:hover:bg-input/50 font-medium text-white cursor-pointer h-10 rounded-md"
-                    onClick={() => setShowMore(!showMore)}
-                  >
-                    {showMore ? "Show Less" : "Show More"}
-                  </Button>
-                </div>
-              )}
-            </div>
-
+            <AnimeSynopsis
+              synopsis={anime.synopsis}
+              shouldShowMore={shouldShowMore}
+              showMore={showMore}
+              setShowMore={setShowMore}
+              synopsisRef={synopsisRef}
+            />
             <AnimeInfoCardGroup anime={anime} />
-
             <StudiosInfo studios={anime.studios} />
             <ProducersInfo producers={anime.producers} />
             <GenresInfo genres={anime.genres} />
@@ -198,6 +181,42 @@ const AnimeStatsBadges = ({ anime }: { anime: Anime }) => {
     </div>
   );
 };
+
+const AnimeSynopsis = ({
+  synopsis,
+  shouldShowMore,
+  showMore,
+  setShowMore,
+  synopsisRef,
+}: {
+  synopsis: string;
+  shouldShowMore: boolean;
+  showMore: boolean;
+  setShowMore: React.Dispatch<React.SetStateAction<boolean>>;
+  synopsisRef: React.RefObject<HTMLParagraphElement | null>;
+}) => (
+  <div className="flex flex-col gap-2">
+    <TypographyP
+      ref={synopsisRef}
+      className={`text-justify whitespace-pre-line line-clamp-7 ${
+        showMore && "line-clamp-none"
+      }`}
+    >
+      {synopsis}
+    </TypographyP>
+
+    {shouldShowMore && (
+      <div className="flex flex-col items-center">
+        <Button
+          className="font-inter dark:bg-input/30 dark:hover:bg-input/50 font-medium text-white cursor-pointer h-10 rounded-md"
+          onClick={() => setShowMore(!showMore)}
+        >
+          {showMore ? "Show Less" : "Show More"}
+        </Button>
+      </div>
+    )}
+  </div>
+);
 
 const AnimeInfoCardGroup = ({ anime }: { anime: Anime }) => (
   <div className="grid grid-cols-2 md:grid-cols-3 gap-4 my-6">
