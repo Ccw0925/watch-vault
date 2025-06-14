e7baca35959c47d252e9ffa7a5a1163fcb5f87f2
Ccw <kidoccw@gmail.com>
2025-06-14 16:41:21 +0800

    feat(frontend): Added metadata title template

--- frontend/src/app/animes/page.tsx
+++ frontend/src/app/animes/page.tsx
@@ -1,62 +1,11 @@
-"use client";
-import AnimeGridGroup from "@/components/anime/AnimeGridGroup";
-import { TypographyH1 } from "@/components/ui/typography";
-import { useAnimes } from "@/hooks/api/animeHooks";
-import Image from "next/image";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import React, { useState } from "react";
+import React from "react";
+import AllAnimesPage from "./AllAnimesPage";
+import { Metadata } from "next";
 
-const AllAnimesPage = () => {
-  const queryParams = useSearchParams();
-  const rating = queryParams.get("rating");
-  const genres = queryParams.get("genres");
-  const page = queryParams.get("page");
-  const pageInt = parseInt(page ?? "1");
-
-  const [sort, setSort] = useState(queryParams.get("sort") ?? "desc");
-  const [orderBy, setOrderBy] = useState(
-    queryParams.get("orderBy") ?? "members"
-  );
-
-  const { data, isLoading, isPlaceholderData } = useAnimes({
-    rating,
-    genres,
-    page: pageInt,
-    orderBy,
-    sort,
-  });
-  const animes = data?.data;
-  const totalPages = data?.pagination.last_visible_page;
-
-  return (
-    <div className="p-5 relative">
-      <div className="absolute top-3 left-5 hidden sm:block h-[90px] w-[160px]">
-        <Link href="/">
-          <Image
-            src="/logo-transparent.png"
-            alt="Logo"
-            width={160}
-            height={90}
-            priority
-          />
-        </Link>
-      </div>
-
-      <TypographyH1 className="text-center mb-5">All Animes</TypographyH1>
-
-      <AnimeGridGroup
-        animes={animes}
-        isLoading={isLoading || isPlaceholderData}
-        pageInt={pageInt}
-        totalPages={totalPages}
-        orderBy={orderBy}
-        sort={sort}
-        setOrderBy={setOrderBy}
-        setSort={setSort}
-      />
-    </div>
-  );
+export const metadata: Metadata = {
+  title: "All Animes",
 };
 
-export default AllAnimesPage;
+const Page = async () => <AllAnimesPage />;
+
+export default Page;
