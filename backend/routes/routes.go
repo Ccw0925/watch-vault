95b5e47f5df1e3b79b7a517127607a1ac0179f16
Ccw <kidoccw@gmail.com>
2025-07-07 14:52:10 +0800

    ci: Set GIN to Release mode on Production

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"context"
+	"os"
 
 	"cloud.google.com/go/firestore"
 	"github.com/Ccw0925/watch-vault/internal/jikan"
@@ -14,6 +15,12 @@ import (
 
 func SetupRoutes(client *firestore.Client, ctx context.Context) *gin.Engine {
 	// Initialize Gin router
+	if os.Getenv("DEBUG") == "true" {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
 	r := gin.Default()
 	r.Use(cors.Default())
 
